42f65dea155db3e23fab61e68114a204663b8d8c
Sani <sani.mitkov.chandarov@gmail.upm.es>
2021-03-10 13:51:53 +0100

    muchas cosas

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,17 +3,77 @@
 
 #include <iostream>
 
-int main()
+#include "freeglut.h"
+
+//los callback, funciones que seran llamadas automaticamente por la glut
+//cuando sucedan eventos
+//NO HACE FALTA LLAMARLAS EXPLICITAMENTE
+void OnDraw(void); //esta funcion sera llamada para dibujar
+void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla
+
+float radio_esfera = 2.0f;
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	//Inicializar el gestor de ventanas GLUT
+	//y crear la ventana
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 600);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("MiJuego");
+
+	//habilitar luces y definir perspectiva
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(40.0, 800 / 600.0f, 0.1, 150);
+
+	//Registrar los callbacks
+	glutDisplayFunc(OnDraw);
+	glutTimerFunc(25, OnTimer, 0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer()
+	glutKeyboardFunc(OnKeyboardDown);
+
+	//pasarle el control a GLUT,que llamara a los callbacks
+	glutMainLoop();
+
+	return 0;
+}
+void OnDraw(void)
+{
+	//Borrado de la pantalla
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Para definir el punto de vista
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0.0, 10, 20,  // posicion del ojo
+		0.0, 0, 0.0,      // hacia que punto mira  (0,0,0)
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)
+
+	//aqui es donde hay que poner el código de dibujo
+	glutSolidSphere(radio_esfera, 20, 20);
+
+
+
+	//no borrar esta linea ni poner nada despues
+	glutSwapBuffers();
+}
+void OnKeyboardDown(unsigned char key, int x_t, int y_t)
+{
+	//poner aqui el código de teclado
+	if (key == '+' && radio_esfera < 3)
+		radio_esfera += 0.5f;
+	if (key == '-' && radio_esfera > 1)
+		radio_esfera -= 0.5f;
+	glutPostRedisplay();
+
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+void OnTimer(int value)
+{
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+}
